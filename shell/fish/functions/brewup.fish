5b3fc4f67f210bde8690dff5db8c76f0939f026e
jschwrtz <jay.schwartz@sparkdemand.io>
2018-05-22 22:01:03 -0500

    SAVEPOINT

--- shell/fish/functions/brewup.fish
+++ shell/fish/functions/brewup.fish
@@ -1,3 +1,4 @@
-function brewup  --description 'update brew & all packages, casks, and apps'
-	brew update; brew outdated; brew upgrade --force-bottle --cleanup; brew cu -a -y; mas upgrade; brew cask cleanup; brew cleanup; brew prune; brew doctor;
+# Defined in /var/folders/jr/5blkp97x441_cj_thkkc6v2m0000gn/T//fish.ZNnhDT/brewup.fish @ line 2
+function brewup --description 'update brew & all packages, casks, and apps'
+	brew update; brew outdated; brew upgrade --cleanup; brew cu --all --yes --cleanup; mas outdated; mas upgrade; brew cask cleanup; brew cleanup; brew prune; brew doctor;
 end
