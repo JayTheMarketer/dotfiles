bbf3748b2a311f0c8f150154116aca6e20d1a875
jschwrtz <jay.schwartz@sparkdemand.io>
2018-05-25 14:38:27 -0500

    SAVEPOINT

--- shell/fish/functions/ofd.fish
+++ shell/fish/functions/ofd.fish
@@ -1,3 +1,8 @@
+# Defined in /var/folders/z7/n40sfdt53cd8d_130wz7s79h0000gp/T//fish.qm8Nie/ofd.fish @ line 2
 function ofd
-  open -a Finder $PWD
+	if test (count $argv) -gt 0
+    open -a Finder $argv
+  else
+    open -a Finder $PWD
+	end
 end
