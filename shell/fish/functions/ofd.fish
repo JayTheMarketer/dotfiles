e67930672af2a7d02b56fd7ec6009f92e69623f1
jschwrtz <jay.schwartz@sparkdemand.io>
2018-05-31 14:32:18 -0500

    :sparkles: add new fish functions

--- shell/fish/functions/ofd.fish
+++
@@ -1,8 +0,0 @@
-# Defined in /var/folders/z7/n40sfdt53cd8d_130wz7s79h0000gp/T//fish.qm8Nie/ofd.fish @ line 2
-function ofd
-	if test (count $argv) -gt 0
-    open -a Finder $argv
-  else
-    open -a Finder $PWD
-	end
-end
