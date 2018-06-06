61ea4e655506f062d94cd8b6df0012b99b2c2d0e
jschwrtz <jay.schwartz@sparkdemand.io>
2018-06-06 11:09:45 -0500

    :construction: SAVEPOINT

--- shell/fish/functions/brewgraph.fish
+++ shell/fish/functions/brewgraph.fish
@@ -1,3 +1,5 @@
+# Defined in /var/folders/z7/n40sfdt53cd8d_130wz7s79h0000gp/T//fish.4fis4R/brewgraph.fish @ line 2
 function brewgraph --description 'graph homebrew dependencies (depends on martido/brew-graph and graphviz)'
-	brew graph --installed --highlight-leaves | fdp -Tpng -o "$HOME/Downloads/graph.png"
+	brew graph --installed --highlight-leaves | fdp -Tpng -o "$HOME/Downloads/graph.png";
+	open "$HOME/Downloads/graph.png"
 end
