2574dc0c2833c7b0f01b9b20254f6d6066b1ca45
jschwrtz <jay.schwartz@sparkdemand.io>
2018-06-12 17:04:18 -0500

    :construction: SAVEPOINT

--- shell/fish/functions/dockerkill.fish
+++ shell/fish/functions/dockerkill.fish
@@ -1,3 +1,5 @@
+# Defined in /var/folders/z7/n40sfdt53cd8d_130wz7s79h0000gp/T//fish.0ReFEa/dockerkill.fish @ line 2
 function dockerkill --description 'stop all docker containers, remove all docker containers, remove all docker images'
-	docker kill (docker ps -q); and docker rm (docker ps -a -q); and docker rmi (docker images -q)
+	docker kill (docker ps -q);
+  docker rm (docker ps -a -q); and docker rmi (docker images -q)
 end
