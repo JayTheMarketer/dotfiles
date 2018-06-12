e7e2c303476ad0a4bed16380cc3099c59aa0e1a2
jschwrtz <jay.schwartz@sparkdemand.io>
2018-06-12 17:16:47 -0500

    :construction: SAVEPOINT

--- golang/install.sh
+++ golang/install.sh
@@ -2,24 +2,25 @@
 
 export GOPATH="$HOME/.go"
 
-if [ "$(which go)" ] && [ -n "$GOPATH" ]; then
-	mkdir -p "$GOPATH/bin" "$GOPATH/src/github.com/"
+if [ "$(command -v go)" ] && [ -n "$GOPATH" ]; then
+  mkdir -p "$GOPATH/bin" "$GOPATH/src/github.com/"
 
-	packages="
+  packages="
     github.com/alecthomas/gometalinter
+    github.com/nsf/gocode
     golang.org/x/tools/cmd/cover
     golang.org/x/tools/cmd/godoc
-    github.com/nsf/gocode
     golang.org/x/tools/cmd/guru
     golang.org/x/tools/cmd/goimports
     golang.org/x/tools/cmd/gorename
-    github.com/golang/lint/golint
+    golang.org/x/lint/golint
   "
 
-	for pkg in $packages; do
-		go get -u "$pkg"
-	done
-	"$GOPATH"/bin/gometalinter --install --update
+  for pkg in $packages; do
+    go get -u "$pkg"
+  done
+
+  "$GOPATH"/bin/gometalinter --install --update
 fi
 
 export GOROOT=`go env GOROOT`
