ca4b0a7d9ec17f1cfc0155641b63708e4a18c105
jschwrtz <jay.schwartz@sparkdemand.io>
2018-08-24 21:37:29 -0500

    :wrench: update vscode settings

--- editor/vscode/install_extensions.sh
+++ editor/vscode/install_extensions.sh
@@ -1,92 +1,70 @@
-#!/bin/sh
+#!/usr/bin/env bash
 
 # from `code --list-extensions`
 extensions=(
-  AndersEAndersen.html-class-suggestions
-  DavidAnson.vscode-markdownlint
-  EditorConfig.EditorConfig
-  HookyQR.minify
-  P-de-Jong.vscode-html-scss
-  PeterJausovec.vscode-docker
-  Zignd.html-css-class-completion
-  aaron-bond.better-comments
-  abusaidm.html-snippets
-  akamud.vscode-theme-onedark
-  alefragnani.Bookmarks
-  alefragnani.project-manager
-  alexdima.copy-relative-path
-  anseki.vscode-color
-  attilabuti.vscode-mjml
-  bierner.color-info
-  castwide.solargraph
-  cdsama.shell-snippets
-  christian-kohler.npm-intellisense
-  christian-kohler.path-intellisense
-  chrmarti.regex
-  CoenraadS.bracket-pair-colorizer
-  cssho.vscode-svgviewer
-  dbaeumer.vscode-eslint
-  deerawan.vscode-dash
-  digitalstreamio.mjml-syntax
-  donjayamanne.jquerysnippets
-  DotJoshJohnson.xml
-  dzannotti.vscode-babel-coloring
-  eamodio.gitlens
-  ecmel.vscode-html-css
-  eg2.vscode-npm-script
-  esbenp.prettier-vscode
-  felixfbecker.php-debug
-  felixfbecker.php-intellisense
-  formulahendry.auto-close-tag
-  formulahendry.auto-rename-tag
-  formulahendry.code-runner
-  foxundermoon.shell-format
-  hdg.live-html-previewer
-  humy2833.ftp-simple
-  idleberg.icon-fonts
-  ikappas.phpcs
-  jinsihou.diff-tool
-  KnisterPeter.vscode-github
-  lunaryorn.fish-ide
-  mkaufman.HTMLHint
-  mrmlnc.vscode-autoprefixer
-  mrmlnc.vscode-scss
-  ms-mssql.mssql
-  ms-python.python
-  ms-vscode.Go
-  ms-vscode.sublime-keybindings
-  msjsdiag.debugger-for-chrome
-  naumovs.color-highlight
-  octref.vetur
-  persoderlind.vscode-phpcbf
-  PKief.material-icon-theme
-  pranaygp.vscode-css-peek
-  rbbit.typescript-hero
-  rebornix.Ruby
-  redhat.vscode-yaml
-  ritwickdey.LiveServer
-  ryu1kn.partial-diff
-  shardulm94.trailing-spaces
-  shd101wyy.markdown-preview-enhanced
-  skyapps.fish-vscode
-  spywhere.guides
-  tanato.vscode-gulp
-  thekalinga.bootstrap4-vscode
-  timonwong.shellcheck
-  tungvn.wordpress-snippet
-  vilicvane.es-quotes
-  WallabyJs.quokka-vscode
-  wayou.vscode-todo-highlight
-  wmaurer.change-case
-  xabikos.JavaScriptSnippets
+	AndersEAndersen.html-class-suggestions
+	HookyQR.minify
+	P-de-Jong.vscode-html-scss
+	TeddyDD.fish
+	Zignd.html-css-class-completion
+	aaron-bond.better-comments
+	abusaidm.html-snippets
+	alefragnani.project-manager
+	alexdima.copy-relative-path
+	anseki.vscode-color
+	castwide.solargraph
+	christian-kohler.npm-intellisense
+	christian-kohler.path-intellisense
+	CoenraadS.bracket-pair-colorizer
+	cssho.vscode-svgviewer
+	DavidAnson.vscode-markdownlint
+	dbaeumer.vscode-eslint
+	deerawan.vscode-dash
+	eamodio.gitlens
+	ecmel.vscode-html-css
+	eg2.vscode-npm-script
+	esbenp.prettier-vscode
+	felixfbecker.php-debug
+	felixfbecker.php-intellisense
+	formulahendry.auto-close-tag
+	formulahendry.auto-rename-tag
+	formulahendry.code-runner
+	foxundermoon.shell-format
+	ikappas.phpcs
+	KnisterPeter.vscode-github
+	lunaryorn.fish-ide
+	mgmcdermott.vscode-language-babel
+	mkaufman.HTMLHint
+	mrmlnc.vscode-autoprefixer
+	mrmlnc.vscode-scss
+	ms-python.python
+	ms-vscode.sublime-keybindings
+	msjsdiag.debugger-for-chrome
+	naumovs.color-highlight
+	naumovs.theme-oceanicnext
+	octref.vetur
+	PeterJausovec.vscode-docker
+	PKief.material-icon-theme
+	pranaygp.vscode-css-peek
+	rebornix.ruby
+	redhat.vscode-yaml
+	ritwickdey.LiveServer
+	ryu1kn.partial-diff
+	shardulm94.trailing-spaces
+	shd101wyy.markdown-preview-enhanced
+	skyapps.fish-vscode
+	spywhere.guides
+	timonwong.shellcheck
+	travisthetechie.write-good-linter
+	WallabyJs.quokka-vscode
+	xabikos.JavaScriptSnippets
 )
 
 for variant in "code" "code-insiders"; do
-  if hash $variant 2>/dev/null; then
-    echo "Installing ${#extensions[@]} extensions for $variant"
-    for extension in "${extensions[@]}"; do
-      "$variant" --install-extension "$extension"
-    done
-  fi
+	if hash $variant 2>/dev/null; then
+		echo "Installing ${#extensions[@]} extensions for $variant"
+		for extension in "${extensions[@]}"; do
+			"$variant" --install-extension "$extension"
+		done
+	fi
 done
-
