da6d3eabbdc9ef76d9bfb74eab4a486e7fb4cbba
JayTheMarketer <jay.schwartz@sparkdemand.io>
2018-09-06 08:59:19 -0500

    :construction: SAVEPOINT

--- shell/fish/functions/updateall.fish
+++ shell/fish/functions/updateall.fish
@@ -1,4 +1,4 @@
-# Defined in /var/folders/z7/n40sfdt53cd8d_130wz7s79h0000gp/T//fish.5KYV5k/updateall.fish @ line 2
+# Defined in /var/folders/z7/n40sfdt53cd8d_130wz7s79h0000gp/T//fish.6DYxlZ/updateall.fish @ line 2
 function updateall --description 'update everything (macOS)'
-	sudo softwareupdate -i -a; brewup; yarnup; gemup; composerup;
+	brewup; yarnup; gemup; composerup; sudo softwareupdate --install --all --restart;
 end
