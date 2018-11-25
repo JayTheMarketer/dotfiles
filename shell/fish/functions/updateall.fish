aa5cea7ee7d00cdce0bebe66bbc53a49adb65845
JayTheMarketer <jay.schwartz@sparkdemand.io>
2018-11-24 19:02:19 -0600

    :recycle: change from yarn to npm

--- shell/fish/functions/updateall.fish
+++ shell/fish/functions/updateall.fish
@@ -1,4 +1,4 @@
-# Defined in /var/folders/z7/n40sfdt53cd8d_130wz7s79h0000gp/T//fish.EoZWkZ/updateall.fish @ line 2
+# Defined in /var/folders/z7/n40sfdt53cd8d_130wz7s79h0000gp/T//fish.unfi2r/updateall.fish @ line 2
 function updateall --description 'update everything (macOS)'
-	sudo softwareupdate --install --all; brewup; yarnup; gemup; composerup;
+	sudo softwareupdate --install --all; brewup; npmup; gemup; composerup;
 end
