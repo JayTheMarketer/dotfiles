d8e0efdeb4b0bc85bb0bdc09db432edd482eccd7
JayTheMarketer <jay.schwartz@sparkdemand.io>
2018-12-06 13:02:17 -0600

    :construction: SAVEPOINT

--- shell/fish/functions/npmup.fish
+++ shell/fish/functions/npmup.fish
@@ -1,4 +1,4 @@
-# Defined in /var/folders/z7/n40sfdt53cd8d_130wz7s79h0000gp/T//fish.CcJGwE/npmup.fish @ line 2
+# Defined in /var/folders/z7/n40sfdt53cd8d_130wz7s79h0000gp/T//fish.SxLbwC/npmup.fish @ line 2
 function npmup --description 'upgrade npm and all global packages'
-	brew upgrade node; sudo npm install -g npm; npm outdated; npm update -g
+	brew upgrade node; npm update -g npm; npm outdated; npm update -g
 end
