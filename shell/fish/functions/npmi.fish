f17d24acbd9ad20badc93e569399b2ca00b49db9
JayTheMarketer <jay.schwartz@sparkdemand.io>
2018-12-14 15:10:28 -0600

    :zap: add npm fish functions and configs for npm and eslint

---
+++ shell/fish/functions/npmi.fish
@@ -0,0 +1,3 @@
+function npmi --description 'install an npm package globally'
+	npm install -g
+end
---
+++ shell/fish/functions/npml.fish
@@ -0,0 +1,3 @@
+function npml --description 'list all global top-level npm packages'
+	npm list -g --depth=0
+end
