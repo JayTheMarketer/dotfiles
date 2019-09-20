235034cadc54614acc4d1b2c230fc1d546d456c5
Jay Schwartz <jay.schwartz@sparkdemand.io>
2019-09-19 22:27:36 -0500

    remove brew prune

--- shell/fish/functions/brewup.fish
+++ shell/fish/functions/brewup.fish
@@ -1,4 +1,4 @@
 # Defined in /var/folders/jr/5blkp97x441_cj_thkkc6v2m0000gn/T//fish.I4KOm7/brewup.fish @ line 2
 function brewup --description 'update brew & all packages, casks, and apps'
-	brew update; brew outdated; brew upgrade --cleanup; brew cu --all --yes --quiet; mas outdated; mas upgrade; brew cleanup; brew prune; brew doctor;
+	brew update; brew outdated; brew upgrade; brew cu --all --yes --quiet; mas outdated; mas upgrade; brew cleanup; brew doctor;
 end
