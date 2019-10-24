1471037c46cb1c8c07f8c309d25bcdc08c7028f1
JayTheMarketer <jay.schwartz@herodigital.com>
2019-10-23 20:28:45 -1000

    :zap: improving homebrew functions

--- shell/fish/functions/rebrew.fish
+++ shell/fish/functions/rebrew.fish
@@ -1,4 +1,9 @@
+# Defined in /var/folders/2m/xvrkknqj1kd8g9t3m6fm0sr40000gq/T//fish.fb55LN/rebrew.fish @ line 2
 function rebrew --description 'a more thorough option to brew reinstall'
-  brew uninstall $argv
-  brew install $argv
+	if count $argv > /dev/null
+    echo 'No package specified'
+  else
+    brew uninstall $argv
+    brew install $argv
+  end
 end
