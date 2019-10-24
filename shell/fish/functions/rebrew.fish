eec153cafcfc0017bb4bdca5ef0a1cec9bc82cb5
JayTheMarketer <jay.schwartz@herodigital.com>
2019-10-23 20:34:51 -1000

    :zap: improving homebrew functions

--- shell/fish/functions/rebrew.fish
+++ shell/fish/functions/rebrew.fish
@@ -1,9 +1,9 @@
-# Defined in /var/folders/2m/xvrkknqj1kd8g9t3m6fm0sr40000gq/T//fish.fb55LN/rebrew.fish @ line 2
+# Defined in /var/folders/2m/xvrkknqj1kd8g9t3m6fm0sr40000gq/T//fish.OAIZ9y/rebrew.fish @ line 2
 function rebrew --description 'a more thorough option to brew reinstall'
 	if count $argv > /dev/null
-    echo 'No package specified'
-  else
     brew uninstall $argv
     brew install $argv
+  else
+    echo 'No package specified'
   end
 end
--- shell/fish/functions/recask.fish
+++ shell/fish/functions/recask.fish
@@ -1,9 +1,9 @@
-# Defined in /var/folders/2m/xvrkknqj1kd8g9t3m6fm0sr40000gq/T//fish.1LIQCM/recask.fish @ line 2
+# Defined in /var/folders/2m/xvrkknqj1kd8g9t3m6fm0sr40000gq/T//fish.UOVdGR/recask.fish @ line 2
 function recask --description 'a more thorough option to brew cask reinstall'
 	if count $argv > /dev/null
-    echo 'No package specified'
-  else
     brew cask uninstall $argv
     brew cask install $argv
+  else
+    echo 'No package specified'
   end
 end
