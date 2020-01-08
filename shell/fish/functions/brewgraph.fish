2446179b382b6b96f284b4d4433f5ac0ed053a6a
JayTheMarketer <jay.schwartz@herodigital.com>
2020-01-08 12:09:40 -0600

    :construction: general update

--- shell/fish/functions/brewgraph.fish
+++ shell/fish/functions/brewgraph.fish
@@ -1,5 +1,5 @@
-# Defined in /var/folders/z7/n40sfdt53cd8d_130wz7s79h0000gp/T//fish.4fis4R/brewgraph.fish @ line 2
+# Defined in /var/folders/2m/xvrkknqj1kd8g9t3m6fm0sr40000gq/T//fish.mE0FRl/brewgraph.fish @ line 2
 function brewgraph --description 'graph homebrew dependencies (depends on martido/brew-graph and graphviz)'
-	brew graph --installed --highlight-leaves | fdp -Tpng -o "$HOME/Downloads/graph.png";
-	open "$HOME/Downloads/graph.png"
+	brew graph --installed --highlight-leaves | fdp -Tpng -o "$HOME/Downloads/homebrew-graph.png";
+	open "$HOME/Downloads/homebrew-graph.png"
 end
